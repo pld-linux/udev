d86bca47e48f1529f63c5e257e378e29f074d9fd
Arkadiusz Miśkiewicz <arekm@maven.pl>
2004-11-01 22:03:30 +0000

    - faster version (arekm && areq)
    Changed files:
        udev-check-cdrom.sh -> 1.2

--- udev-check-cdrom.sh
+++ udev-check-cdrom.sh
@@ -8,14 +8,25 @@ found=0
 
 [ -e /proc/sys/dev/cdrom/info ] || exit 1
 
-/bin/cat /proc/sys/dev/cdrom/info | {
-	IFS=":"
-	while read name val; do
-	    name=$(echo "$name" | xargs)
-	    val=$(echo "$val" | xargs)
-	    newname="${name%%${what}}"
-	    [ "$name" = "drive name" -a "$val" = "$sp" ] && found=1
-	    [ "$found" -eq 1 -a "$val" = "1" -a "$name" != "$newname" ] && exit 0
-	done
-	exit 1
-}
+/bin/awk "BEGIN { ok=0 } 
+		/drive name:/ {
+			gsub(/^.*:/, NIL)
+			for (i=1;i<=NF;i++) {
+			    c[\$i]=i
+			}
+		}
+		/^Can.*$what:/ {
+			gsub(/^.*:/, NIL, \$0)
+			q=int(c[\"$sp\"])
+			if (\$q == 1) {
+			    ok=1
+			}
+		}
+		END {
+			if (ok) {
+			    exit 0 
+			} else { 
+			    exit 1 
+		        }
+		}
+" /proc/sys/dev/cdrom/info
